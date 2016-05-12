48a144c5d5185469ecacfaa9a98245d1d2007d79
Jiri Zoudun <jiri.zoudun.dev@gmail.com>
2016-05-12 23:36:53 +0200

    Pretified code in temperature reading script.

--- src/temp_read.rb
+++ src/temp_read.rb
@@ -1,53 +1,86 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 # TODO: Clean the code!
 
-SLEEP_TIME = 60*10 # Log temperature every 10 mins
+SLEEP_TIME = 0.5#60*10 # Log temperature every x seconds.
+SENSORS_DIR = '/sys/bus/w1/devices' # Directory where sensors are readed.
+OUTPUT_DIRECTORY = './temp_readings/'
 
-# Function for printing temperature from given file content.
+# Configure interface.
+def init_sensor
+  `modprobe w1-gpio`
+  `modprobe w1-therm`
+end 
+
+# Function for getting temperature from given file content.
+# Temperature is in content with format 't=26500' which means 26.5.
+# Content contains some other information that aren't important for us.
 def temp(content)
   index = content.index 't='
   content[index+2, content.length].to_i / 1000.0
 end
 
-p 'Temperature reading from DS18B20 sensor..'
+# Returns sensor name for reading.
+def sensor_name
+  base_dir = Dir.new SENSORS_DIR
 
-# Configure interface.
-`modprobe w1-gpio`
-`modprobe w1-therm`
+  # Directory for given sensor starts with prefix '28' and then there's
+  # some identifier.
+  sensor_names = base_dir.grep /^28*/ # Find sensors/
 
-base_dir = Dir.new '/sys/bus/w1/devices'
-
-sensor_names = base_dir.grep /^28*/ # Find sensors/
-
-if sensor_names.count <= 0
-  STDERR.puts 'No sensor found. Aborting..'
-  abort
-end
-
-# Use first sensor only.
-# TODO: Use more than one sensor.
-sensor_name = sensor_names.first
-
-while
-  
-  content = File.read( base_dir.path + '/' +
-              sensor_name + '/w1_slave')
-              
-  if content.include? 'YES'
-    t = temp(content)
-    p t
-
-    open('temperature.out', 'a') do |f|
-      f << Time.now.to_s + ';' + t.to_s + "\n"
-    end
-    
-    sleep SLEEP_TIME
-  else
-    p 'No successful reading..'
-    sleep 0.2
+  if sensor_names.count <= 0
+    STDERR.puts 'No sensor found. Aborting..'
+    abort
   end
 
+  # Use first sensor only.
+  # TODO: Use more than one sensor.
+  base_dir.path + '/' + sensor_names.first
+end
+
+# Writes a temperature to the file as backup solution.
+# Files are written by days (which can cause problems when RPI has no
+# internet connection -> no time).
+def print_to_file(temp)
+  time = Time.new
+
+  FileUtils.mkdir_p OUTPUT_DIRECTORY
+  
+  open(OUTPUT_DIRECTORY + time.strftime("%Y%m%d") + '.temp', 'a') do |f|
+    f << time.strftime("%H%M%S") + ';' + temp.to_s + "\n"
+  end
+end
+
+# SCRIPT
+p 'Temperature reading from DS18B20 sensor..'
+init_sensor
+
+while
+  # Read the content of the file which contains current temperature from
+  # the sensor.
+  content = File.read(sensor_name + '/w1_slave')
+
+  # 'YES' in the content indicates that temperature is readable. For
+  # more info see DS18B20 datasheet or some tutorial for RPI and DS18B20.
+  unless content.include? 'YES'
+    p 'No successful reading..'
+    sleep 0.2
+    next
+  end
+
+  # Read a temperature from the file content.
+  t = temp(content)
+
+  p t
+
+  # Printing temperature to the file (backup variant maybe?)
+  print_to_file t
+
+  # TODO: Client side sending of temperature.
+
+  sleep SLEEP_TIME
 end
 
 
